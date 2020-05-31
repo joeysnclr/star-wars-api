b83cbdf958b4ae97a12cba9d1f252f015751862b
Joey Sinclair <joey.sinclair02@gmail.com>
2020-05-31 20:53:49 +0000

    enabled cors

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const cors = require('cors')
 const app = express();
-const fs = require('fs');
 const dataFile = require('./data.json')
 
-
 let data = dataFile;
+
+app.use(cors())
 
 
 app.get('/api', function (req, res) {
